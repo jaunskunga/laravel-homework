af9b210872ba1a35c9986930a8e1d8ed4e643426
jaunskunga <jaunskunga@gmail.com>
2022-05-23 20:21:11 +0300

    adding my example

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/articles', function () {
+    $articles = Article::get();
+    dd($articles);
 });
